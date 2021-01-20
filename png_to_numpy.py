80a147501dcd554bea5935e795ddf565a96fb563
CM-Kim <min1003k@gmail.com>
2021-01-20 22:29:02 +0900

    png -> numpy 코드 완성

--- png_to_numpy.py
+++ png_to_numpy.py
@@ -1,18 +1,46 @@
 import numpy as np
-from PIL import Image
 import os
 import cv2
 
-# TODO
-# 1. mask 이용해서 label 행렬 만들기
-# 2. mat_to_numpy 참고해서 labels / images / masks 만들기
-# 3. 아 전처리 어케하지 ㅋㅋ;
+t1w_images = []
+t2f_images = []
+t2w_images = []
 
-path_dir = 'C:/MRI_Brain/dataset/mask/'
-file_list = os.listdir(path_dir)
+# T1W 이미지들 load
+path_T1W = 'C:/MRI_Brain/dataset/T1W_brain_pic/'
+file_list = os.listdir(path_T1W)
 
-for png in file_list:
-    image = Image.open(path_dir + png)
-    pixel = np.array(image)
-    png = png.split('.')[0]
-    np.save("C:/MRI_Brain/dataset/numpy_mask/" + png, pixel)
+for img in file_list:
+    img_path = path_T1W + img
+    image = cv2.imread(img_path)
+    image = cv2.resize(image, dsize=(512, 512), interpolation=cv2.INTER_CUBIC)
+    t1w_images.append(image)
+
+# T2F 이미지들 load
+path_T2F = 'C:/MRI_Brain/dataset/T2F_brain_pic/'
+file_list = os.listdir(path_T2F)
+
+for img in file_list:
+    img_path = path_T2F + img
+    image = cv2.imread(img_path)
+    image = cv2.resize(image, dsize=(512, 512), interpolation=cv2.INTER_CUBIC)
+    t2f_images.append(image)
+
+# T2W 이미지들 load
+path_T2W = 'C:/MRI_Brain/dataset/T2W_brain_pic/'
+file_list = os.listdir(path_T2W)
+
+for img in file_list:
+    img_path = path_T2W + img
+    image = cv2.imread(img_path)
+    image = cv2.resize(image, dsize=(512, 512), interpolation=cv2.INTER_CUBIC)
+    t2w_images.append(image)
+
+# numpy array로 변환 후 저장
+t1w_images = np.array(t1w_images)
+t2f_images = np.array(t2f_images)
+t2w_images = np.array(t2w_images)
+
+np.save("C:/MRI_Brain/dataset/numpy/t1w_images.npy", t1w_images)
+np.save("C:/MRI_Brain/dataset/numpy/t2f_images.npy", t2f_images)
+np.save("C:/MRI_Brain/dataset/numpy/t2w_images.npy", t2w_images)
