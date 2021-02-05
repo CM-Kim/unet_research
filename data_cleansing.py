3f60ba9a4ead87318dfae2627f4489f258429007
CM-Kim <55427945+CM-Kim@users.noreply.github.com>
2021-02-05 17:48:46 +0900

    ROI가 공백인 MRI 데이터 쌍을 삭제

---
+++ data_cleansing.py
@@ -0,0 +1,29 @@
+# 쓸모없는 사진을 지워보아요~
+import cv2
+import numpy as np
+import os
+
+def is_img_empty(img):
+    if np.any(img >= 1):
+        return False
+    return True
+
+PATH = "./MRI_T1T2T2F_ROI/"
+file_list = os.listdir(PATH)
+
+delete_list = []
+
+for img in file_list:
+    if img.endswith('_ROI.png'):
+        IMG_PATH = PATH + img
+        image = cv2.imread(IMG_PATH)
+
+        if is_img_empty(image):
+            delete_list.append(img[:24] + "ROI.png")
+            delete_list.append(img[:24] + "T1W.png")
+            delete_list.append(img[:24] + "T2F.png")
+            delete_list.append(img[:24] + "T2W.png")
+
+for target in delete_list:
+    target_path = PATH + target
+    os.remove(target_path)
